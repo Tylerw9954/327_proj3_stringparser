71c7c1308ddb44aeb9dde378872f08874ff13a42
Tyler Warburton <tylerw9954@gmail.com>
2019-11-02 21:31:33 -0400

    95 still

--- StringParserClass.cpp
+++ StringParserClass.cpp
@@ -1,9 +1,3 @@
-/*
- * StringParserClass.cpp
- *
- *  Created on: Oct 8, 2017
- *      Author: keith
- */
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
 #include "../327_proj3_test/includes/StringParserClass.h"
@@ -39,7 +33,6 @@ StringParserClass::~StringParserClass() {
 int StringParserClass::setTags(const char *pStart, const char *pEnd) {
 	//Quickly end the method if receiving NULL
 	if (pStart == NULL || pEnd == NULL) {
-		cout << "start or end == null";
 		return ERROR_TAGS_NULL;
 	}
 	//get integers with the size of the pointers to be copied
@@ -50,20 +43,9 @@ int StringParserClass::setTags(const char *pStart, const char *pEnd) {
 	pStartTag = new char[slength - 1];
 	pEndTag = new char[elength - 1];
 
-
 	//Copy the data from the first pointer to our global variable
-
-	//Copy the data from the first pointer to our global variable
-
 	strcpy(pStartTag, pStart);
 	strcpy(pEndTag, pEnd);
-
-	//TODO make these equal. They are not for some reason
-	cout << pStart << endl;
-	cout << pStartTag << endl;
-
-	cout << pEnd << endl;
-	cout << pEndTag << endl;
 
 	return SUCCESS;
 }
@@ -80,27 +62,16 @@ int StringParserClass::getDataBetweenTags(char *pDataToSearchThru,
 	//First clears myVector
 	myVector.clear();
 
-	char *output = pDataToSearchThru;
-
-
 	if (pStartTag == NULL || pEndTag == NULL) {
-		cout << "start tag or end tag == null";
 		return ERROR_TAGS_NULL;
 	}
-
-	//TODO clean myvector
 	if (pDataToSearchThru == NULL) {
-		cout << "data to search through == null";
 		return ERROR_DATA_NULL;
 	}
-
-
 
 	//Gotta make the scope of this big
 	int index = strlen(pDataToSearchThru);
 	int i = 0;
-	int indexToStartReadingAt = 0;
-	int indexToStopReadingAt = 0;
 	bool lookingForStartTag = true;
 	int returnVal;
 	string returnString = "";
@@ -118,8 +89,6 @@ int StringParserClass::getDataBetweenTags(char *pDataToSearchThru,
 
 			//start doing real work here
 			if (returnVal == SUCCESS) {
-				cout << "Found ittt ";
-
 				if (!lookingForStartTag) {
 					myVector.push_back(returnString);
 					lookingForStartTag = true;
@@ -127,38 +96,17 @@ int StringParserClass::getDataBetweenTags(char *pDataToSearchThru,
 				} else {		//not looking for start tag
 					lookingForStartTag = false;
 					i += strlen(pStartTag);
-
 				}
 			}
-			if (returnVal == FAIL) {
-				cout << "Didint Foind it";
-			}
-			if (returnVal == ERROR_TAGS_NULL) {
-				cout << "thruw a fukoin error";
-			}
 		}
-
 
 		if(!lookingForStartTag){
-			cout << pDataToSearchThru[i];
 			returnString += pDataToSearchThru[i];
 		}
-
 		i++;
 		index--;
 	}
 
-	cout << endl;
-
-	//At this point we should have the starting point of the char array and the endpoint
-	indexToStopReadingAt = 100;
-	while (indexToStartReadingAt != indexToStopReadingAt) {
-		//string adding(1, indexToStartReadingAt[indexToStartReadingAt]);
-		//myVector.push_back(adding);
-		indexToStartReadingAt++;
-	}
-
-	//TODO fix return
 	return SUCCESS;
 }
 
@@ -172,7 +120,6 @@ void StringParserClass::cleanup() {
 	pStartTag = NULL;
 	pEndTag = NULL;
 	areTagsSet = false;
-
 }
 
 //Searches a string starting at pStart for pTagToLookFor
@@ -183,39 +130,27 @@ void StringParserClass::cleanup() {
 int StringParserClass::findTag(char *pTagToLookFor, char *&pStart,
 		char *&pEnd) {
 	if (pStartTag == NULL || pEndTag == NULL) {
-		cout << "start tag or end tag == null";
 		return ERROR_TAGS_NULL;
 	}
 
 	int startindex = strlen(pTagToLookFor);
 	int x = 0;
 	bool startHere = true;
-
+//loops through the chars
 	while (startindex != 0) {
 		//validate that the current char is equal to the same char in the startTag
 		if (pStart[x] != pTagToLookFor[x]) {
 			//if not then break out of loop and dont start looking for values here
 			startHere = false;
-			//not sure to have here or not
-			cout << pStart[x];
-			cout << " didn't equal ";
-			cout << pTagToLookFor[x] << endl;
 			break;
 		}
-
-		//Should go '<' == '<', 't' == 't', 'o' == 'o', '>' == '>'
-		cout << pStart[x];
-		cout << pTagToLookFor[x] << endl;
-
 		x++;
 		startindex--;
 	}
 	if (startHere) {
-		//TODO Might be off by one
 		//THE CODE HERE SEEMS TO WORK OF THE SET TAGS WORKS PROPERLY
 		pEnd = &pStart[x];
 		return SUCCESS;
 	}
-
 	return FAIL;
 }
